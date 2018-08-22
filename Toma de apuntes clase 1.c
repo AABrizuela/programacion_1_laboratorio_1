32424aae91e5107e7402411db9e0a6bc35529e4a
Adrián <40859100+AABrizuela@users.noreply.github.com>
2018-08-22 12:22:43 -0300

    Add files via upload

---
+++ Toma de apuntes clase 1.c
@@ -0,0 +1,55 @@
+
+    //varialbes
+
+    int lalala;
+
+    //para numeros decimales se usa
+
+    float 69.420;
+
+    //para mostrar
+
+    printf("----");
+
+    //para concatenar se usa la coma
+
+    printf("variable1: %d - variable2: %d",var1,var2);
+
+    /*
+    "%d" es para un entero en sistema decimal
+    "%f" es para un float o un doble
+    "%c" es para 1 caracter
+    "%s" es para una cadena de caracteres o un array
+    "%i" se puede usar igual que "%d" pero para otros sistemas
+
+    Tengo que respetar la mascara con respecto al dato que guarda la varialbe
+
+    para tomar datos se usa
+    */
+
+    scanf("___",&___);
+
+    /*
+    solo cuando es un vector del tipo "char" va sin el "&"
+
+    para char es "%c" (de char)
+    para string es "%s" (de string)
+
+    Para C no existen booleanos, para C "false == 0" y "true != 0". Cero solamente es falso.
+    Cualquier valor distinto de cero es verdadero, incluso letras.
+    D:\Brizuela.Adrian\holamundo\holamundo.cbp*/
+
+    /*
+    Cuando declaro una varialbe o una constante en C nunca tiene que ir con un numero como primer caracter
+
+    Constantes en mayusculas, variables en minusculas
+
+    Ejemplo
+    */
+
+    #define IVA 21
+    #define PI 3.14
+    #define LETRA 'A'
+    #define PALABRA "Hola"
+
+    /*
